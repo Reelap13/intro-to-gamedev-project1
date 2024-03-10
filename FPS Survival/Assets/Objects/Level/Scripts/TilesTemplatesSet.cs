73e3e7525ac714b6f800b56f4251977503b9593d
Mikhail Stepanov <mi.stepanov@innopolis.university>
2024-03-10 16:19:31 +0300

    LevelGenerator
    Implement the simplest version of procedural generation

---
+++ FPS Survival/Assets/Objects/Level/Scripts/TilesTemplatesSet.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilesTemplatesSet : MonoBehaviour
+{
+    [SerializeField] private TemplateTile[] _tiles;
+
+    private float _frequencies_sum;
+
+    private void Awake()
+    {
+        CalculateFrequenciesSum();    
+    }
+
+    public GameObject GetRandomTilePrefab()
+    {
+        float rand = Random.Range(0.0f, _frequencies_sum);
+        return GetTile(rand).TilePrefab;
+    }
+    private void CalculateFrequenciesSum()
+    {
+        _frequencies_sum = 0;
+        foreach (var tile in _tiles)
+            _frequencies_sum += tile.Frequency;
+    }
+    private TemplateTile GetTile(float value)
+    {
+        foreach (var tile in _tiles)
+        {
+            value -= tile.Frequency;
+            if (value < 0.0f)
+                return tile;
+        }
+        return _tiles[0];
+    }
+}
