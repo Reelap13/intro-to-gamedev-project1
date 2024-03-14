c3a82d15eaee9bc81a7b783cd29d6693c7f70539
Mikhail Stepanov <mi.stepanov@innopolis.university>
2024-03-14 03:26:34 +0300

    Tiles
    Create some new tile for level generator

--- FPS Survival/Assets/Objects/Level/Scripts/LevelGenerator.cs
+++ FPS Survival/Assets/Objects/Level/Scripts/LevelGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.AI.Navigation;
@@ -12,19 +13,28 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] private Vector3 _tile_size = Vector3.one;
 
     private Dictionary<Pair<int, int>, GameObject> _level;
+    private Vector3 _center;
 
     public void GenerateLevel()
     {
+        int center_x = Convert.ToInt32(_level_size.x / 2 - 0.5);
+        int center_y = Convert.ToInt32(_level_size.y / 2 - 0.5);
+        _center = new Vector3(center_x, 0, center_y);
+
         _level = new Dictionary<Pair<int, int>, GameObject>();
         for (int i = 0; i < _level_size.x; ++i)
         {
             for (int j = 0; j < _level_size.y; ++j)
             {
+                if (i == center_x && j == center_y)
+                    continue;
                 GameObject tile_pref = _tiles_set.GetRandomTilePrefab();
                 GameObject tile = CreateTile(tile_pref, i, j);
                 _level.Add(new Pair<int, int>(i, j), tile);
             }
         }
+
+        _level.Add(new Pair<int, int>(center_x, center_y), CreateTile(_tiles_set.DefaultTile, center_x, center_y));
         _surface.BuildNavMesh();
     }
 
@@ -37,4 +47,5 @@ public class LevelGenerator : MonoBehaviour
 
         return tile;
     }
+    public Vector3 Center { get { return new Vector3(_center.x * _tile_size.x, 0, _center.z * _tile_size.z); } }
 }
